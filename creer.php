21ffef48cfb28575b27756228eb7508caef2fce1
0xZales <rozalesamiral@gmail.com>
2023-02-27 19:04:03 +0000

    GH

--- creer.php
+++ creer.php
@@ -1,5 +1,5 @@
 <?php
-if (!empty($_POST['hangar'])&& empty($_POST['id'])) {
+if (!empty($_POST['hangar']) && empty($_POST['id'])) {
     $hangar = $_POST['hangar'];
     $occupe = $_POST['occupe'];
     $sql = "INSERT INTO hangar(numero_hangar,occupe) VALUES(:hangar,:occupe)";
@@ -17,7 +17,6 @@ if (!empty($_POST['hangar'])&& empty($_POST['id'])) {
 
 }
 
-
 if (!empty($_POST['id'])) {
     $occupe = $_POST['occupe'];
     $numero_hangar = $_POST["hangar"];
@@ -27,12 +26,13 @@ if (!empty($_POST['id'])) {
     if (
         $statement->execute([
             ':numero_hangar' => $numero_hangar,
-            'occupe'=>$occupe,
+            'occupe' => $occupe,
             ':id' => $_POST['id']
         ])
     ) {
 
-        $message = "MIs a jour effectuee";
+        $message = "Mis a jour effectuee";
+        header('location:/Hangar');
     }
     // $id = $pdo->lastInsertId();
 
@@ -55,7 +55,7 @@ require('header.php')
                 </div>
             <?php endif; ?>
             <form action="" method="post">
-                <input type="hidden" name="id" id="id" class="form-control w-50 mx-auto my-4" value="<?php if (isset($_GET['id'])) {
+                <input type="text" name="id" id="id" class="form-control w-50 mx-auto my-4" value="<?php if (isset($_GET['id'])) {
                     echo $_GET['id'];
                 } else {
                     echo "";
@@ -63,11 +63,14 @@ require('header.php')
 
                 <div class="form-group">
                     <label for="hangar">Numero hangar</label>
-                    <input type="text" name="hangar" id="hangar" class="form-control w-50 mx-auto my-4" value="<?php if (isset($_GET['id'])) {
+                    <input type="text" name="hangar" id="hangar" class="form-control w-50 mx-auto my-4" value="<?php
+
+                    if (isset($_GET['id'])) {
                         $sql = "SELECT * FROM hangar WHERE id=:id";
                         $statement = $db->prepare($sql);
                         $statement->execute([':id' => $_GET['id']]);
                         $data = $statement->fetch(PDO::FETCH_OBJ);
+
                         echo $data->numero_hangar;
                     } else {
                         echo "";
@@ -76,9 +79,21 @@ require('header.php')
                 <div class="form-group">
                     <select class="form-select text-center w-50 mx-auto my-2" name="occupe"
                             aria-label="Default select example" id="occupe">
-                        <option selected>Statut d'occupation</option>
-                        <option value="Oui">Oui</option>
-                        <option value="Non">Non</option>
+                        </option>
+                        <option <?php if (!isset($_GET['id'])) {
+                            echo "selected='true'";
+                        } ?>>Statut d'occupation</option>
+                        <option value="Oui" <?php if (isset($_GET['id']) && $data->occupe == "Oui") {
+                            echo "selected='true'";
+                        } else {
+                            echo "";
+                        } ?>>Oui</option>
+                        <option value="Non" <?php if (isset($_GET['id']) && $data->occupe == "Non") {
+                            echo "selected='true'";
+                        } else {
+                            echo "";
+                        } ?>>Non
+
                     </select>
                 </div>
                 <div class="form-group my-2">
